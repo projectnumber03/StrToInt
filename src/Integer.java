82ee7eef9d14e53580e663e8a91fa7779eecb296
DShilov <abarakus@bk.ru>
2019-02-07 19:06:23 +0300

    Initial commit

---
+++ src/Integer.java
@@ -0,0 +1,24 @@
+public class Integer {
+    public static int valueOf(String str) throws NumberFormatException {
+        int summ = 0;
+        int k1;
+        int k2;
+        if (str.charAt(0) == '-') {
+            k1 = 1;
+            k2 = -1;
+        } else {
+            k1 = 0;
+            k2 = 1;
+        }
+        for (int i = k1; i < str.length(); i++) {
+            summ += charToInt(str.charAt(i)) * Math.pow(10, str.length() - i - 1);
+        }
+        return k2 * summ;
+    }
+
+    public static int charToInt(char chr) throws NumberFormatException {
+        if ((int) chr > 48 && (int) chr < 57) {
+            return (int) chr - 48;
+        } else throw new NumberFormatException();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException {
+        String str = "123";
+        System.out.println(Integer.valueOf(str));
+    }
+}
