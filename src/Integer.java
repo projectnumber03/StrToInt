dfa666245f13c69c60cdd253480f6b864ea01a4a
DShilov <abarakus@bk.ru>
2019-02-12 15:27:16 +0300

    Char conversion

--- src/Integer.java
+++ src/Integer.java
@@ -1,9 +1,10 @@
 public class Integer {
+
     public static int valueOf(String str) throws NumberFormatException {
         return str.charAt(0) == '-' ? convert(str, 1, -1) : convert(str, 0, 1);
     }
 
-    public static int convert(String str, int k1, int k2){
+    public static int convert(String str, int k1, int k2) {
         int summ = 0;
         for (int i = k1; i < str.length(); i++) {
             summ += charToInt(str.charAt(i)) * Math.pow(10, str.length() - i - 1);
